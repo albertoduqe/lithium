4bc5b741eba90a866136da924d2b23cd87aeed38
albertoduqe <albertoduqe@gmail.com>
2018-07-31 12:10:18 +0200

    Bootleggers bringup

--- bootleg.mk
+++ bootleg.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/lithium/full_lithium.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_lithium
+PRODUCT_NAME := bootleg_lithium
 PRODUCT_DEVICE := lithium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI MIX
